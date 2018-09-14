5e927487f36c657916c973d7f1b4b5340b68b572
johnson20050 <johnson20050@gmail.com>
2018-09-14 11:35:49 +0800

    add a script to update this repository

---
+++ script/UpdateUnixTool.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+cp -r -f ~/script     ~/Work/github/UnixTools/
+cp    -f ~/.bashrc    ~/Work/github/UnixTools/bashrc
+cp    -f ~/.vimrc     ~/Work/github/UnixTools/vimrc
+cp    -f ~/.tmux.conf ~/Work/github/UnixTools/tmux.conf
